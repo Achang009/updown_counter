8a8240e46389b5daf2e321ee4988304146f2e745
Achang009 <146834990+Achang009@users.noreply.github.com>
2026-01-20 15:48:41 +0800

    Update updown_counter.v

--- updown_counter.v
+++ updown_counter.v
@@ -13,38 +13,34 @@ entity updown_counter is
 end updown_counter;
 
 architecture Behavioral of updown_counter is
-    constant DIV_LIMIT : integer := 25000000; -- 要模擬的話改0
-    signal r_div_cnt   : integer range 0 to DIV_LIMIT := 0;
-    signal f_clk       : std_logic := '0';
 
-    constant max       : STD_LOGIC_VECTOR(3 downto 0) := "1001";
-    signal cnt_up      : STD_LOGIC_VECTOR(3 downto 0) := "0000";
-    signal cnt_down    : STD_LOGIC_VECTOR(3 downto 0) := max;
+    signal bin_cnt : STD_LOGIC_VECTOR(24 downto 0) := (others => '0');
+    signal f_clk   : std_logic := '0';
+
+    constant max      : STD_LOGIC_VECTOR(3 downto 0) := "1001";
+    signal cnt_up     : STD_LOGIC_VECTOR(3 downto 0) := "0000";
+    signal cnt_down   : STD_LOGIC_VECTOR(3 downto 0) := max;
 
 begin
 
     frequency_divider: process(i_clk, i_reset)
     begin
         if i_reset = '1' then
-            r_div_cnt <= 0;
-            f_clk     <= '0';
+            bin_cnt <= (others => '0');
         elsif rising_edge(i_clk) then
-            if r_div_cnt = DIV_LIMIT then
-                r_div_cnt <= 0;
-                f_clk     <= not f_clk;
-            else
-                r_div_cnt <= r_div_cnt + 1;
-            end if;
+            bin_cnt <= bin_cnt + 1;
         end if;
     end process frequency_divider;
+
+    f_clk <= bin_cnt(24);
 
     up_counter: process(f_clk, i_reset)
     begin
         if i_reset = '1' then
-            cnt_up <= 0;
+            cnt_up <= "0000";
         elsif rising_edge(f_clk) then
             if cnt_up >= max then
-                cnt_up <= 0;
+                cnt_up <= "0000";
             else
                 cnt_up <= cnt_up + 1;
             end if;
@@ -56,7 +52,7 @@ begin
         if i_reset = '1' then
             cnt_down <= max;
         elsif rising_edge(f_clk) then
-            if cnt_down = 0 then
+            if cnt_down = "0000" then
                 cnt_down <= max;
             else
                 cnt_down <= cnt_down - 1;
@@ -68,4 +64,3 @@ begin
     o_cntdown <= cnt_down;
 
 end Behavioral;
-
